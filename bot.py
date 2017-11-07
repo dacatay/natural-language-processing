a518698262c3fdd70c7f3617f2922922d830b81f
Daniel K Acatay <daniel.acatay@googlemail.com>
2017-11-07 11:13:06 +0100

    initial commit

---
+++ bot.py
@@ -0,0 +1,21 @@
+import newspaper
+import feedparser as fp
+import json
+from newspaper import Article
+from time import mktime
+from datetime import datetime
+
+
+# Set the limit for number of articles to download
+LIMIT = 4
+
+data = {}
+data['newspapers'] = {}
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
